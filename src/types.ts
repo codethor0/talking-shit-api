d8cb5aa45cecf9d74c3f2c5bc70e22247069a23b
Thor Thor <37605399+codethor0@users.noreply.github.com>
2026-09-10 11:15:55 -0500

    release: prepare v0.4.0 (#17)

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export const API_VERSION = "v1" as const;
-export const SERVICE_VERSION = "0.3.0" as const;
+export const SERVICE_VERSION = "0.4.0" as const;
 
 export const CATEGORIES = [
   "general",
